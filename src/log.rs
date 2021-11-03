f74ef92b4e56d90cb943a841fb8fd34d6af625ae
Tumukunde Arnold <artumukunde@hotmail.com>
2021-11-03 17:21:57 +0300

    Update log.rs

--- src/log.rs
+++ src/log.rs
@@ -6,10 +6,10 @@ use ethereum_types::{H160, H256};
 use rlp_derive::{RlpDecodable, RlpEncodable};
 
 
-#[derive(Clone, Debug, PartialEq, Eq, RlpEncodable, RlpDecodable)]
+#[derive(Clone, Debug, PartialEq, Eq, RlpEncodable, RlpDecodable, TypeInfo)]
 #[cfg_attr(
 	feature = "with-codec",
-	derive(codec::Encode, codec::Decode, TypeInfo)
+	derive(codec::Encode, codec::Decode)
 )]
 #[cfg_attr(feature = "with-serde", derive(serde::Serialize, serde::Deserialize))]
 pub struct Log {
