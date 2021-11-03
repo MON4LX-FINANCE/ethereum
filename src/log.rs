8f5593481221b8bbbffba3094f20ae9aebce7471
Tumukunde Arnold <artumukunde@hotmail.com>
2021-11-03 16:26:33 +0300

    scale-info for log

--- src/log.rs
+++ src/log.rs
@@ -1,8 +1,9 @@
 use alloc::vec::Vec;
 use rlp_derive::{RlpEncodable, RlpDecodable};
 use ethereum_types::{H160, H256};
+use scale_info::TypeInfo;
 
-#[derive(Clone, Debug, PartialEq, Eq, RlpEncodable, RlpDecodable)]
+#[derive(Clone, Debug, PartialEq, Eq, RlpEncodable, RlpDecodable, TypeInfo)]
 #[cfg_attr(feature = "codec", derive(codec::Encode, codec::Decode))]
 pub struct Log {
     pub address: H160,
